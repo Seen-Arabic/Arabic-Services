cf1a1793edd26c31312ae4042b27a1c7e16da35a
Abdelrahman Bayoumi <abdelrahmanbayoumi1@gmail.com>
2023-12-10 23:42:12 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,54 +1,62 @@
-const cacheName = 'services-v2.2.6';
+const CACHE_NAME = 'services-v2.2.10';
 const staticAssets = [
 	'./',
-	'./404.html',
 	'./index.html',
+	'./assets/css/main.css',
+	'./assets/js/main.js',
+	'./assets/js/arabic-services.umd.js',
+	// images
 	'./assets/images/logo-transparent.svg',
 	'./assets/images/logo-circle.svg',
 	'./assets/images/logo.svg',
-	'./assets/css/home.css',
-	'./assets/css/main.css',
-	'./assets/js/oldArabic.js',
-	'./assets/js/letterToWord.js',
-	'./letter-to-word/index.html',
-	'./old-arabic/index.html',
+	// logo icons
+	'./assets/images/logo_x48.png',
+	'./assets/images/logo_x72.png',
+	'./assets/images/logo_x96.png',
+	'./assets/images/logo_x128.png',
+	'./assets/images/logo_x192.png',
+	'./assets/images/logo_x384.png',
+	'./assets/images/logo_x512.png',
+
+	'./404.html',
+	'./manifest.json',
+	'./robots.txt',
 ];
 
-self.addEventListener('install', async (e) => {
-	const cache = await caches.open(cacheName);
-	await cache.addAll(staticAssets);
-	return self.skipWaiting();
+self.addEventListener('install', (event) => {
+	// Perform install steps
+	event.waitUntil(
+		caches.open(CACHE_NAME).then((cache) => {
+			console.log('Opened cache');
+			return cache.addAll(staticAssets);
+		}),
+	);
 });
 
-self.addEventListener('activate', (e) => {
-	self.clients.claim();
+self.addEventListener('activate', (event) => {
+	const cacheWhitelist = [CACHE_NAME];
+
+	event.waitUntil(
+		caches.keys().then((cacheNames) => {
+			return Promise.all(
+				cacheNames.map((cacheName) => {
+					if (cacheWhitelist.indexOf(cacheName) === -1) {
+						return caches.delete(cacheName);
+					}
+				}),
+			);
+		}),
+	);
 });
 
-self.addEventListener('fetch', async (e) => {
-	const req = e.request;
-	const url = new URL(req.url);
-
-	if (url.origin === location.origin) {
-		e.respondWith(cacheFirst(req));
-	} else {
-		e.respondWith(networkAndCache(req));
-	}
+self.addEventListener('fetch', (event) => {
+	event.respondWith(
+		caches.match(event.request).then((response) => {
+			// Cache hit - return response
+			if (response) {
+				return response;
+			}
+			return fetch(event.request);
+		}),
+	);
 });
-
-async function cacheFirst(req) {
-	const cache = await caches.open(cacheName);
-	const cached = await cache.match(req);
-	return cached || fetch(req);
-}
-
-async function networkAndCache(req) {
-	const cache = await caches.open(cacheName);
-	try {
-		const fresh = await fetch(req);
-		await cache.put(req, fresh.clone());
-		return fresh;
-	} catch (e) {
-		const cached = await cache.match(req);
-		return cached;
-	}
-}
